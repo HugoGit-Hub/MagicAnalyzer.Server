27fdcb7a21f40c4c3aa665a5c9909002455bd649
hugo <hugo.decuq.34@icloud.com>
2024-10-12 19:25:13 +0200

    Create Domain library

---
+++ Domain/Cards/Card.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Base;
+using Domain.Decks;
+
+namespace Domain.Cards;
+
+public class Card : Entity<int>
+{
+    public Guid CardGuid { get; }
+    
+    public Deck Deck { get; } = null!;
+
+    private Card(int id, Guid cardGuid) : base(id)
+    {
+        CardGuid = cardGuid;
+    }
+    
+    public static Card Create(int id, Guid cardGuid)
+    {
+        return new Card(id, cardGuid);
+    }
+}
---
+++ Domain/Decks/Deck.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Base;
+using Domain.Cards;
+
+namespace Domain.Decks;
+
+public class Deck : Entity<int>
+{
+    public string Name { get; }
+
+    public ICollection<Card> Cards { get; } = [];
+
+    private Deck(int id, string name) : base(id)
+    {
+        Name = name;
+    }
+
+    public static Deck Create(int id, string name)
+    {
+        return new Deck(id, name);
+    }
+}
