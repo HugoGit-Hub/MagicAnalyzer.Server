1c27c8ff1face1e0f9db4952d4d1579ebd55d252
hugo <hugo.decuq.34@icloud.com>
2024-10-13 00:39:53 +0200

    Deck controller

--- Api/Controllers/DeckController.cs
+++ Api/Controllers/DeckController.cs
@@ -1,45 +1,70 @@
+using Application.Decks.AddCard;
+using Application.Decks.CreateDeck;
+using Application.Decks.DeleteCard;
+using Application.Decks.DeleteDeck;
+using Application.Decks.GetAllDecks;
+using Application.Decks.GetDeck;
 using Domain.Decks;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Api.Controllers;
 
 [ApiController]
 [Route("[controller]")]
-public class DeckController : ControllerBase
+public class DeckController(ISender sender) : ControllerBase
 {
     [HttpPost(nameof(Create))]
-    public Deck Create(string name)
+    public async Task<ActionResult<string>> Create(string name, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        var result = await sender.Send(new CreateDeckCommand(name), cancellationToken);
+
+        return Ok(result);
     }
     
     [HttpGet("Get/{id:int}")]
-    public Deck Get(int id)
+    public async Task<ActionResult<GetDeckResponse>> Get(int id, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        var result = await sender.Send(new GetDeckQuery(id), cancellationToken);
+        
+        return Ok(result);
     }
     
     [HttpGet(nameof(GetAll))]
-    public IEnumerable<Deck> GetAll()
+    public async Task<ActionResult<IEnumerable<GetAllDecksResponse>>> GetAll(CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
-    }
-    
-    [HttpPut("AddCard/{id:int}")]
-    public Deck AddCard(int id)
-    {
-        throw new NotImplementedException();
-    }
-    
-    [HttpPut("DeleteCard/{id:int}")]
-    public Deck DeleteCard(int id)
-    {
-        throw new NotImplementedException();
+        var result = await sender.Send(new GetAllDecksQuery(), cancellationToken);
+        
+        return Ok(result);
     }
     
     [HttpDelete("Delete/{id:int}")]
-    public Deck Delete(int id)
+    public async Task<ActionResult<string>> Delete(int id,CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        var result = await sender.Send(new DeleteDeckCommand(id), cancellationToken);
+        
+        return Ok(result);
+    }
+    
+    [HttpPost("AddCard/{deckId:int}/{cardGuid:guid}")]
+    public async Task<ActionResult<AddCardResponse>> AddCard(
+        int deckId,
+        Guid cardGuid,
+        CancellationToken cancellationToken)
+    {
+        var result = await sender.Send(new AddCardCommand(deckId, cardGuid), cancellationToken);
+        
+        return Ok(result);
+    }
+    
+    [HttpDelete("DeleteCard/{deckId:int}/{cardId:int}")]
+    public async Task<ActionResult<DeleteCardResponse>> DeleteCard(
+        int deckId,
+        int cardId,
+        CancellationToken cancellationToken)
+    {
+        var result = await sender.Send(new DeleteCardCommand(deckId, cardId), cancellationToken);
+        
+        return Ok(result);
     }
 }
