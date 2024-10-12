542e9370b3b675405bbb79f0cf803711031f29cf
hugo <hugo.decuq.34@icloud.com>
2024-10-13 00:39:10 +0200

    Delete a deck

---
+++ Application/Decks/DeleteDeck/DeleteDeckCommand.cs
@@ -0,0 +1,5 @@
+﻿using MediatR;
+
+namespace Application.Decks.DeleteDeck;
+
+public record DeleteDeckCommand(int Id) : IRequest<string>;
---
+++ Application/Decks/DeleteDeck/DeleteDeckCommandHandler.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Decks;
+using MediatR;
+
+namespace Application.Decks.DeleteDeck;
+
+public class DeleteDeckCommandHandler(IDeckRepository deckRepository) : IRequestHandler<DeleteDeckCommand, string>
+{
+    public async Task<string> Handle(DeleteDeckCommand command, CancellationToken cancellationToken)
+    {
+        var deck = await deckRepository.Get(command.Id, cancellationToken);
+        var result = await deckRepository.Delete(deck, cancellationToken);
+        
+        return result.Name;
+    }
+}
